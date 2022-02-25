b5d29f03ac83590bfab3372622b655306bfd044d
sparkHou <1>
2022-02-26 00:04:59 +0800

    init

--- springMVC-DEMO2/src/main/java/com/atguigu/mvc/controller/TestController.java
+++ springMVC-DEMO2/src/main/java/com/atguigu/mvc/controller/TestController.java
@@ -1,13 +1,30 @@
 package com.atguigu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
+        System.out.print("用print输出的第一条信息");
         return "index";
+    }
+
+    @RequestMapping(
+            value = {"/testRequestMapping", "/test"},
+            method = {RequestMethod.POST}
+    )
+    public String test() {
+        return "index";
+    }
+
+
+    @GetMapping("/testGetMapping")
+    public String testGetMapping() {
+        return "success";
     }
 }
