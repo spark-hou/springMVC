373e31b77ebda94c587d13d1ef6d6fbe61f85685
sparkHou <1>
2022-03-01 00:02:47 +0800

    init

---
+++ springMVC-DEMO2/src/main/java/com/atguigu/mvc/bean/User.java
@@ -0,0 +1,8 @@
+package com.atguigu.mvc.bean;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String password;
+}
--- springMVC-DEMO2/src/main/java/com/atguigu/mvc/controller/TestController.java
+++ springMVC-DEMO2/src/main/java/com/atguigu/mvc/controller/TestController.java
@@ -2,8 +2,11 @@ package com.atguigu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TestController {
@@ -25,6 +28,31 @@ public class TestController {
 
     @GetMapping("/testGetMapping")
     public String testGetMapping() {
+        return "success";
+    }
+
+    @GetMapping("/testServletAPI")
+    public String testServletAPI(HttpServletRequest request) {
+        String username = request.getParameter("userName");
+        String password = request.getParameter("password");
+        System.out.println(username + password);
+        return "success";
+    }
+
+    @GetMapping("/testParam")
+    public String testParam(
+            String userName,
+            String password,
+            @RequestHeader("host") String host) {
+        System.out.println(userName + password + " testParam" + host);
+        return "success";
+    }
+//p32
+    @GetMapping("/testpojo")
+    public String testpojo(
+
+            ) {
+        System.out.println(user);
         return "success";
     }
 }
